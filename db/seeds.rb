5518f8f1583e28deb4973189d0a5852c86eba107
princecoder51002 <83036763+princecoder51002@users.noreply.github.com>
2021-05-28 20:24:50 +0530

    add user and message resorce

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "prince", password: "password")
+User.create(username: "hardik", password: "password")
+User.create(username: "saksham", password: "password")
+User.create(username: "rishabh", password: "password")
+User.create(username: "jasmine", password: "password")
